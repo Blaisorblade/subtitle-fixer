da7b1c2743a27373eeedfd69af8aa5f3f2e98d8c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2012-07-22 16:59:00 +0200

    Reindent

--- fixSubtitles.scala
+++ fixSubtitles.scala
@@ -32,55 +32,55 @@ object FixSubtitles extends App {
 //    val origFps = 25.0
 //    val targetFps = 23.976
 //    val timeMultiplier = 1 //origFps / targetFps //? Or the inverse?
-  val timeMultiplier = (origFps, targetFps) match {
-    case (Some(orig), Some(target)) =>
-      orig / target //? Or the inverse?
-    case _ => 1
-  }
-
-  //val fName = "David+Lynch+-+Twin+Peaks+-+Fire+Walk+With+Me+%281992%29 - orig.srt"
-  def input =
-    //Source.fromFile(fName)
-    Source.stdin
-
-  def parse(time: String): Int = {
-    val Time = "(..):(..):(..),(...)".r
-    time match {
-      case Time(h, m, s, ms) => ((((h.toInt * 60) + m.toInt) * 60) + s.toInt) * 1000 + ms.toInt
+    val timeMultiplier = (origFps, targetFps) match {
+      case (Some(orig), Some(target)) =>
+        orig / target //? Or the inverse?
+      case _ => 1
     }
-  }
 
-  def printDate(tm: (Int, Int, Int, Int)) = {
-    val (h, m, s, ms) = tm
-    "%02d:%02d:%02d,%03d" format (h, m, s, ms)
-  }
+    //val fName = "David+Lynch+-+Twin+Peaks+-+Fire+Walk+With+Me+%281992%29 - orig.srt"
+    def input =
+      //Source.fromFile(fName)
+      Source.stdin
 
-  def split(cumTimeMs: Int) = {
-    def divMod(quot: Int, div: Int) = (quot / div, quot % div)
-    val cumMs = cumTimeMs
-    val (cumS, ms) = divMod(cumMs, 1000)
-    val (cumM, s) = divMod(cumS, 60)
-    val (h, m) = divMod(cumM, 60)
-    (h, m, s, ms)
-  }
-
-  def adjust(timeMs: Int): Int =
-    (timeMs * timeMultiplier).toInt + deltaMs
-
-  def fixedLine(line: String) = {
-    val TimeLine = "(.*)-->(.*)".r
-
-    def fixedTimestampLine(inpDate: String) = printDate(split(adjust(parse(inpDate.trim))))
-    line match {
-      case TimeLine(start, end) =>
-        "%s --> %s" format (fixedTimestampLine(start), fixedTimestampLine(end))
-      case _ => line
+    def parse(time: String): Int = {
+      val Time = "(..):(..):(..),(...)".r
+      time match {
+        case Time(h, m, s, ms) => ((((h.toInt * 60) + m.toInt) * 60) + s.toInt) * 1000 + ms.toInt
+      }
     }
-  }
 
-  for (line <- input.getLines()) {
-    println(fixedLine(line))
-  }
+    def printDate(tm: (Int, Int, Int, Int)) = {
+      val (h, m, s, ms) = tm
+      "%02d:%02d:%02d,%03d" format (h, m, s, ms)
+    }
+
+    def split(cumTimeMs: Int) = {
+      def divMod(quot: Int, div: Int) = (quot / div, quot % div)
+      val cumMs = cumTimeMs
+      val (cumS, ms) = divMod(cumMs, 1000)
+      val (cumM, s) = divMod(cumS, 60)
+      val (h, m) = divMod(cumM, 60)
+      (h, m, s, ms)
+    }
+
+    def adjust(timeMs: Int): Int =
+      (timeMs * timeMultiplier).toInt + deltaMs
+
+    def fixedLine(line: String) = {
+      val TimeLine = "(.*)-->(.*)".r
+
+      def fixedTimestampLine(inpDate: String) = printDate(split(adjust(parse(inpDate.trim))))
+      line match {
+        case TimeLine(start, end) =>
+          "%s --> %s" format (fixedTimestampLine(start), fixedTimestampLine(end))
+        case _ => line
+      }
+    }
+
+    for (line <- input.getLines()) {
+      println(fixedLine(line))
+    }
   } getOrElse {
       // arguments are bad, usage message will have been displayed
   }
